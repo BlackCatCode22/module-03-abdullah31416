0d7559891d1333c09da90e079d888f640696cd47
abdullah31416 <156399261+abdullah31416@users.noreply.github.com>
2024-02-18 23:58:41 -0800

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,16 +1,14 @@
-public class Cat extends Animal {
-    private String breed;
+public class Animal {
+    private String name;
+    private int age;
+    private String species;
+    private boolean isMammal;
 
-    public Cat(String name, int age, String species, boolean isMammal, String breed) {
-        super(name, age, species, isMammal);
-        this.breed = breed;
-    }
-
-    public String getBreed() {
-        return breed;
-    }
-
-    public void setBreed(String breed) {
-        this.breed = breed;
+    public Animal(String name, int age, String species, boolean isMammal) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+        this.isMammal = isMammal;
     }
 }
+
