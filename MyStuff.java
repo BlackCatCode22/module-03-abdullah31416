034e359b061005b87f00015c583eb72863910c95
abdullah31416 <156399261+abdullah31416@users.noreply.github.com>
2024-02-18 23:43:39 -0800

    MyStuff.java

---
+++ MyStuff.java
@@ -0,0 +1,22 @@
+public class MyStuff {
+
+     public static void main(String [] args) {
+
+        Driver myDriver = new Driver();
+        myDriver.firstName = "Albert";
+        myDriver.lastName = "Halpert";
+        myDriver.licenseNumber = "1B8DC";
+        myDriver.licenseClass = "B";
+        myDriver.age = 24;
+
+        
+        Driver myDriver2 = new Driver();
+        myDriver2.firstName = "Pam";
+        myDriver2.lastName = "Beasley";
+        myDriver2.licenseNumber = "Y9612907";
+        myDriver2.licenseClass = "C";
+        myDriver2.age = 23;
+
+        System.out.println();
+     }
+}
