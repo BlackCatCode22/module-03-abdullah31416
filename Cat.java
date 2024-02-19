d62bd189fb9822fe3c84e0e1706b6abf7e6899b9
abdullah31416 <156399261+abdullah31416@users.noreply.github.com>
2024-02-18 23:57:36 -0800

    Cat.java

---
+++ Cat.java
@@ -0,0 +1,17 @@
+
+public class Cat extends Animal {
+    private String breed;
+
+    public Cat(String name, int age, String species, boolean isMammal, String breed) {
+        super(name, age, species, isMammal);
+        this.breed = breed;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+}
